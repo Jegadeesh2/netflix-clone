563e751c6227466dd5046f91840dd12bb688e516
Jegadeesh2 <sjegadeeshwaran2@gmail.com>
2023-06-06 15:52:33 +0530

    dot env package installed

--- netflix-api/index.js
+++ netflix-api/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv");
 
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+dotenv.config();
+console.log(process.env.ATLAS_URI);
 
 mongoose
   .connect(process.env.ATLAS_URI, {
